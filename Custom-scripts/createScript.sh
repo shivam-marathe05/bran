7b0dc43376c269f18a4a75092da638e52daa7b03
shivam.marathe@juspay.in <shivam.marathe@juspay.in>
2025-04-21 09:13:26 +0530

    updated Stack Creation Script and other configs

---
+++ Custom-scripts/createScript.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "Starting cluster setup..."
+#CONFIGURE THESE PATHS BEFORE RUNNING
+CLUSTER_MANIFEST="$HOME/Documents/Learn/Fampay-dev/bran/Global-K8s-Configs/K8s-configs/cluster-config.yaml"      
+HODR_DIR="$HOME/Documents/Learn/Fampay-dev/hodr"
+BRAN_DIR="$HOME/Documents/Learn/Fampay-dev/bran"
+#K8S_DIR="$HOME/Documents/Learn/Fampay-dev/bran"
+CLUSTER_NAME="fampay-dev"
+
+
+# Create/ensure namespace & core cluster resources"
+KIND_EXPERIMENTAL_PROVIDER=podman kind create cluster --name ${CLUSTER_NAME} --config "${CLUSTER_MANIFEST}"
+#kind create cluster --name ${CLUSTER_NAME} --config "${CLUSTER_MANIFEST}"
+kubectl config use-context "kind-${CLUSTER_NAME}"
+kubectl create namespace fampay-dev
+kubectl config set-context --current --namespace=fampay-dev
+echo "Cluster created!"
+
+# Build container images with Podman
+#podman build -f "${HODR_DIR}/Dockerfile" -t localhost/hodr-app:latest "${HODR_DIR}"
+docker build -f "${HODR_DIR}/Dockerfile" -t hodr-app:latest "${HODR_DIR}"
+docker build -f "${BRAN_DIR}/Dockerfile" -t bran-app:latest   "${BRAN_DIR}"
+echo "All Images build completed!"
+
+#Save images to tarballs
+#podman save localhost/hodr-app:latest -o hodr-app.tar
+docker save hodr-app:latest -o hodr-app.tar
+docker save bran-app:latest -o bran-app.tar
+echo "Save images to tarballs"
+
+#Load images into Kind cluster
+kind load docker-image hodr-app:latest --name "${CLUSTER_NAME}"
+kind load docker-image bran-app:latest --name "${CLUSTER_NAME}"
+echo "All images loaded into Kind cluster nodes!"
+
+#Install Metrics Server
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+kubectl -n kube-system patch deployment metrics-server \
+  --type='json' \
+  -p='[{"op":"add","path":"/spec/template/spec/containers/0/args/-","value":"--kubelet-insecure-tls"}]'
+kubectl -n kube-system rollout restart deployment/metrics-server
+echo "Deployed Metrics Server!"
+
+#Deploy Cluster Autoscaler (dry-run manifests)
+kubectl create -f "Global-K8s-Configs/K8s-configs/ClusterAutoscaler/clusterautoscaler-sa.yaml"
+kubectl create -f "Global-K8s-Configs/K8s-configs/ClusterAutoscaler/clusterautoscaler-role.yaml"
+kubectl create -f "Global-K8s-Configs/K8s-configs/ClusterAutoscaler/clusterautoscaler-rolebinding.yaml"
+kubectl create -f "Global-K8s-Configs/K8s-configs/ClusterAutoscaler/clusterautoscaler-deploy.yaml"
+echo "Deployed ClusterAutoscaler!"
+
+#Install calico for ingress and egress netpol rules
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+kubectl -n kube-system \
+  wait --for=condition=Ready pod \
+  -l k8s-app=calico-node --timeout=180s
+kubectl -n kube-system delete daemonset kindnet
+
+#Deploy NGINX proxy
+kubectl create -f "Global-K8s-Configs/K8s-configs/nginx-cm.yaml"
+kubectl create -f "Global-K8s-Configs/K8s-configs/nginx-deploy.yaml"
+kubectl create -f "Global-K8s-Configs/K8s-configs/nginx-svc.yaml"
+echo "Deployed Nginx Proxy!"
+
+#Deploy Squid proxy
+#kubectl create -f "Global-K8s-Configs/K8s-configs/squid-cm.yaml"
+#kubectl create -f "Global-K8s-Configs/K8s-configs/squid-deploy.yaml"
+#kubectl create -f "Global-K8s-Configs/K8s-configs/squid-svc.yaml"
+#echo "Deployed Squid Proxy!"
+
+#Apply network policies
+kubectl create -f "Global-K8s-Configs/K8s-configs/network-policy-ingress.yaml"
+#kubectl create -f "Global-K8s-Configs/K8s-configs/network-policy-egress.yaml"
+echo "Applied Network Policies!"
+
+#Deploy app secrets/config & workloads"
+kubectl apply -f "k8sConfig-bran/bran-secret.yaml"
+kubectl apply -f "k8sConfig-bran/bran-cm.yaml"
+kubectl apply -f "k8sConfig-bran/bran-deploy.yaml"
+kubectl apply -f "k8sConfig-bran/bran-svc.yaml"
+kubectl apply -f "k8sConfig-bran/bran-hpa.yaml"
+kubectl apply -f "/Users/shivam.marathe/Documents/Learn/Fampay-dev/hodr/K8s-configs-hodr/hodr-deploy.yaml"
+kubectl apply -f "/Users/shivam.marathe/Documents/Learn/Fampay-dev/hodr/K8s-configs-hodr/hodr-svc.yaml"
+kubectl apply -f "/Users/shivam.marathe/Documents/Learn/Fampay-dev/hodr/K8s-configs-hodr/hodr-hpa.yaml"
+echo "Application is Deployed!"
+
+#Deploy monitoring setup
+kubectl create namespace monitoring
+kubectl config set-context --current --namespace=monitoring
+
+echo "K8s cluster \"${CLUSTER_NAME}\" should now be up with metrics, autoscaling, proxies, and both services deployed."
---
+++ README-STACKSETUP.md
@@ -0,0 +1,39 @@
+# FamPay SRE – Quick‑Start Guide
+# Prerequisites
+# 1. Install podman/docker
+# 2. Install kind 
+# 3. Install wget 
+# 4. Install helm
+# 5. Install curl/telnet/ping
+Steps to install podman 4.9 -
+wget https://github.com/containers/podman/releases/download/v4.9.3/podman-installer-macos-arm64.pkg
+Then open directory with finder and install it
+wget https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/39.20240407.3.0/aarch64/fedora-coreos-39.20240407.3.0-qemu.aarch64.qcow2.xz
+podman machine init --image-path fedora-coreos-39.20240407.3.0-qemu.aarch64.qcow2.xz
+podman machine start
+brew install kind
+brew install wget
+brew install helm
+brew install curl , telnet, ping  #for connectivity check
+# For creating things from scratch please run create script 
+# before running script please give access to that file 
+chmod +x createScript.sh
+./createScript.sh
+# How to test ?
+kubectl -n fampay-dev port-forward svc/nginx-proxy 30080:80 
+#Run these commands externally
+curl -s http://localhost:30080/hodr/        # 200
+curl -s http://localhost:30080/bran/   # 200
+curl -s http://localhost:30080/hodr/metrics        # 200
+curl -s http://localhost:30080/bran/metrics      # 200
+# Please run these commands internally
+# Bran ➜ Hodor (should succeed)
+kubectl exec deploy/bran  -- bash 
+curl -vk http://hodr.fampay-dev.svc.cluster.local:8080  # 200
+# Hodor ➜ Bran (should timeout)
+kubectl exec deploy/hodr -- bash 
+curl -vk http://bran.fampay-dev.svc.cluster.local:8000  # timeout
+# Done.
+# In order to update your application , please run below script
+chmod +x updateScript.sh
+./updateScript.sh
